e3a038c542fc45e8fdedf0bb10de6438ab162e83
Hércules S. S. José <hercules@tic.ufrj.br>
2021-07-10 13:31:47 -0300

    Inclusão do Spring e das classes de negócio.

---
+++ oportunidade/src/br/com/hslife/oportunidade/task/BuscarOportunidadesTask.java
@@ -0,0 +1,56 @@
+package br.com.hslife.oportunidade.task;
+
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BuscarOportunidadesTask {
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    
+    public void buscarOportunidades() {
+        System.out.println("Oportunidades salvas com sucesso!");
+
+        /*
+        System.out.println("Iniciando a busca pelas oportunidades... :: Hora local - " + dateTimeFormatter.format(LocalDateTime.now()))
+
+        // Traz as variáveis de ambiente
+        def envvars = System.getenv()
+
+        def telegramBotToken = envvars.get("app.telegram.token")
+        def channelID = envvars.get("app.telegram.channel")
+
+        // Atualiza o cadastro de oportunidades
+        List<String> linkOportunidades = new ArrayList<>()
+        linkOportunidades.add("https://www.gov.br/servidor/pt-br/centrais-de-conteudo/oportunidades/divulgacao/cessao-1/cessao")
+        linkOportunidades.add("https://www.gov.br/servidor/pt-br/centrais-de-conteudo/oportunidades/divulgacao/movimentacao/movimentacao")
+        linkOportunidades.add("https://www.gov.br/servidor/pt-br/centrais-de-conteudo/oportunidades/divulgacao/requisicao/requisicao")
+        linkOportunidades.add("https://www.gov.br/servidor/pt-br/centrais-de-conteudo/oportunidades/divulgacao/ex-territorios/ex-territorios")
+
+        // Busca as oportunidades
+        for (String link : linkOportunidades) {
+            oportunidadeService.buscarOportunidades(link)
+        }
+
+        // Envia a notificação das novas oportunidades
+        oportunidadeService.notificarOportunidades(telegramBotToken, Long.parseLong(channelID))
+
+        // Atualiza o cadastro de concursos
+        List<String> linkConcursos = new ArrayList<>()
+        linkConcursos.add("https://www.pciconcursos.com.br/concursos/")
+        linkConcursos.add("https://www.pciconcursos.com.br/concursos/nacional/")
+        linkConcursos.add("https://www.pciconcursos.com.br/concursos/sudeste/")
+        linkConcursos.add("https://www.pciconcursos.com.br/concursos/sul/")
+        linkConcursos.add("https://www.pciconcursos.com.br/concursos/norte/")
+        linkConcursos.add("https://www.pciconcursos.com.br/concursos/nordeste/")
+        linkConcursos.add("https://www.pciconcursos.com.br/concursos/centrooeste/")
+
+        for (String link : linkConcursos) {
+            concursoService.buscarConcursos(link)
+        }
+
+        System.out.println("Finalizado a busca pelas oportunidades... :: Hora local - " + dateTimeFormatter.format(LocalDateTime.now()))
+        */
+    }
+}
